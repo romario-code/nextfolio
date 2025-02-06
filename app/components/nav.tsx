e3816acefbcfe24540f6d4565f74f5ca7e6d0e0d
Romário <romariojdosantos@gmail.com>
2025-02-06 17:32:45 -0300

    feat: enhance Navbar component with new font integration, improve mobile menu structure, and refine layout for better responsiveness

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -5,7 +5,7 @@ import { useTranslations } from "next-intl";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { MenuIcon } from "lucide-react";
 
-import { inter } from "../_lib/fonts";
+import { syne, inter } from "../_lib/fonts";
 
 import {
   DropdownMenu,
@@ -25,40 +25,45 @@ export function Navbar({ name }: { name: string }) {
   };
   return (
     <div className="flex justify-between flex-wrap items-center mt-4 container">
-      <div className="logo">
-        <Link
-          href="/"
-          className="text-3xl font-semibold tracking-tight hover:bg-accent hover:text-accent-foreground rounded-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 p-1 font-syne"
-        >
-          {metaData.logoText}
-        </Link>
+      <Link
+        href="/"
+        className={`inline-flex text-3xl font-semibold tracking-tight hover:bg-accent hover:text-accent-foreground rounded-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 p-1 font-syne ${syne.className}`}
+      >
+        {metaData.logoText}
+      </Link>
+      <div className="menuMobile flex items-center gap-2 md:hidden">
+        <div className="flex gap-2 items-center">
+          <ThemeSwitch />
+          <LanguageSwitcher />
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MenuIcon
+              size={38}
+              className="flex flex-col items-center gap-2 font-heading text-lg md:hidden"
+            />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-4">
+            <DropdownMenuLabel>Menu</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="flex flex-col items-start">
+              {Object.entries(navItems).map(([path, { name }]) => (
+                console.log('tetse', path),
+                <Link
+                  key={path}
+                  href={path as "/" | "/en" | "/pt"}
+                  className="uppercase text-neutral-600 dark:text-neutral-300 flex flex-col items-center rounded-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground justify-start text-sm py-2"
+                >
+                  {name}
+                </Link>
+              ))}
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <MenuIcon
-            size={32}
-            className="flex flex-col items-center gap-2 font-heading text-lg md:hidden"
-          />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="mr-4">
-          <DropdownMenuLabel>Menu</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="flex flex-col items-start">
-            {Object.entries(navItems).map(([path, { name }]) => (
-              <Link
-                key={path}
-                href={path as "/" | "/en" | "/pt"}
-                className="uppercase text-neutral-600 dark:text-neutral-300 flex flex-col items-center rounded-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground justify-start text-sm py-2"
-              >
-                {name}
-              </Link>
-            ))}
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <nav className="mt-1 sm:mt-0 overflow-scroll md:overflow-auto hidden md:block">
+      <nav className="mt-1 sm:mt-0 overflow-scroll md:overflow-auto hidden md:flex">
         <div className="flex items-center justify-between">
-          <div className="flex flex-row md:ml-auto items-center overflow-scroll sm:overflow-auto">
+          <div className="flex-row md:ml-auto items-center overflow-scroll sm:overflow-auto">
             {Object.entries(navItems).map(([path, { name }]) => (
               <Link
                 key={path}
@@ -69,10 +74,8 @@ export function Navbar({ name }: { name: string }) {
               </Link>
             ))}
           </div>
-          <div className="mx-3">
+          <div className="flex gap-2 items-center">
             <ThemeSwitch />
-          </div>
-          <div>
             <LanguageSwitcher />
           </div>
         </div>
