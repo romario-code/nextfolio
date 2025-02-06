45ee36379bbae96df98359fb6bad5576a7c54208
Romário <romariojdosantos@gmail.com>
2025-02-06 17:41:23 -0300

    feat: update page component with new routing for internationalization, enhance layout, and improve text styling

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { ArrowRight } from "lucide-react";
-import Link from "next/link";
+import { Link } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
 import { Button } from "@/components/ui/button";
 import { syne, inter } from '../_lib/fonts';
@@ -9,26 +9,23 @@ export default function Page() {
   const t = useTranslations('HomePage');
   return (
     <section className="container flex flex-col justify-center h-[70vh] md:h-[calc(100vh-120px)]">
-      <div className="">
-        <h1 className="text-5xl font-bold tracking-tight mb-3">
-          Romário J. Santos
+      <div>
+     <div>
+     </div>
+        <span className={`text-base font-bold text-stone-400 ${syne.className}`}>Romário de Jesus dos Santos</span>
+        <h1 className={`text-2xl sm:text-5xl md:text-7xl lg:text-8xl font-extrabold tracking-tight mb-3 ${syne.className}`}>
+        {t('title')}
         </h1>
-        <h2 className={`text-xl font-semibold dark:text-neutral-50 ${syne.className}`}>
-          {t('about')}
-        </h2>
-        <p className={`text-base mt-1 text-neutral-800 dark:text-neutral-300 font-normal ${inter.className}`}>
-          {t('description')}
-        </p>
+        <h2 className={`text-1xl font-bold dark:text-neutral-50 ${syne.className}`}>Build, Hobby, Learn</h2>
         <Link
-          prefetch={false}
-          href="mailto:romariojs94@proton.me"
+          href={{ pathname: "/projects" } as any}
           className={`inline-flex items-center gap-2 font-normal text-base text-neutral-700 dark:text-neutral-300 ${inter.className}`}
         >
           <Button
             className="mt-5 text-sm flex-shrink-0 font-normal"
             variant="outline"
           >
-            get in touch
+            {t('about-more')}
             <ArrowRight />
           </Button>
         </Link>
